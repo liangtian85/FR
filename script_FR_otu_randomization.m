5acd55091003b6d47e1008000d5d6bf0d89b5483
liangtian85 <70846822+liangtian85@users.noreply.github.com>
2020-11-01 17:51:45 +0800

    Update script_FR_otu_randomization.m

--- script_FR_otu_randomization.m
+++ script_FR_otu_randomization.m
@@ -12,22 +12,22 @@ ab_table_real=ab_table_real(:,sum(ab_table_real>0,1)>5);
 
 % Real taxonomic profile
 dij_real=pdist(ko_table_real,@distfun_WeightedJaccard);
-[SD_real, FD_real, FR_real]=FDFR_Rao_q (ab_table_real, dij_real, 1);
+[SD_real, FD_real, FR_real]=FDFR_Rao_q (ab_table_real, dij_real);
 
 % NULL-composition-1
 Num_strain=size(ab_table_real,1);
 ko_table_relabel=HMP_GCN_table(randperm(length(HMP_IMGid),Num_strain),:);
 ko_table_relabel=ko_table_relabel(:,sum(ko_table_relabel,1)>0);
 dij_relabel=pdist(ko_table_relabel,@distfun_WeightedJaccard);
-[SD_NULL_relabel, FD_NULL_relabel, FR_NULL_relabel]=FDFR_Rao_q (ab_table_real, dij_relabel, 1);
+[SD_NULL_relabel, FD_NULL_relabel, FR_NULL_relabel]=FDFR_Rao_q (ab_table_real, dij_relabel);
 
 % NULL-composition-2
 ab_table_NULL_OTU1=OTU_table_random( ab_table_real, 4 );
-[SD_NULL_OTU1, FD_NULL_OTU1, FR_NULL_OTU1]=FDFR_Rao_q (ab_table_NULL_OTU1, dij_real, 1);
+[SD_NULL_OTU1, FD_NULL_OTU1, FR_NULL_OTU1]=FDFR_Rao_q (ab_table_NULL_OTU1, dij_real);
 
 % NULL-composition-3
 ab_table_NULL_OTU2=OTU_table_random( ab_table_real, 5 );
-[SD_NULL_OTU2, FD_NULL_OTU2, FR_NULL_OTU2]=FDFR_Rao_q (ab_table_NULL_OTU2, dij_real, 1);
+[SD_NULL_OTU2, FD_NULL_OTU2, FR_NULL_OTU2]=FDFR_Rao_q (ab_table_NULL_OTU2, dij_real);
 
 %% Mann-Whitney U test
 p_values_1=signrank( FR_real./ SD_real, FR_NULL_relabel./ SD_NULL_relabel);
